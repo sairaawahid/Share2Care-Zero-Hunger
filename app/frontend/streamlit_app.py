4a23c52e978a79c3574353adb370fc588769a29f
Sairaawahid <sairaawahid@gmail.com>
2025-10-02 13:57:17 +0500

    Update streamlit_app.py

--- app/frontend/streamlit_app.py
+++ app/frontend/streamlit_app.py
@@ -1,7 +1,14 @@
+import sys
+import os
+
+# ✅ Ensure the correct working directory is in sys.path
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 import folium
 from streamlit_folium import st_folium
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 from app.backend.config import (
