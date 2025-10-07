855704bc0ede6057b289df0a24a36dc8c433700e
Sairaawahid <sairaawahid@gmail.com>
2025-10-07 18:44:41 +0500

    Update donations.py

--- app/backend/routes/donations.py
+++ app/backend/routes/donations.py
@@ -1,21 +1,28 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from sqlmodel import Session, select
 from models import Donation
-from database import donations
+from db import get_session
 
 router = APIRouter()
 
+
 @router.post("/donations")
-def create_donation(donation: Donation):
-    donations.append(donation)
+def create_donation(donation: Donation, session: Session = Depends(get_session)):
+    session.add(donation)
+    session.commit()
+    session.refresh(donation)
     return {"message": "Donation added successfully", "donation": donation}
 
+
 @router.get("/donations")
-def get_donations():
+def get_donations(session: Session = Depends(get_session)):
+    donations = session.exec(select(Donation)).all()
     return donations
 
+
 @router.get("/donations/{donation_id}")
-def get_donation(donation_id: int):
-    for d in donations:
-        if d.id == donation_id:
-            return d
-    raise HTTPException(status_code=404, detail="Donation not found")
+def get_donation(donation_id: int, session: Session = Depends(get_session)):
+    donation = session.get(Donation, donation_id)
+    if not donation:
+        raise HTTPException(status_code=404, detail="Donation not found")
+    return donation
